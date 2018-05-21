294f6e3bada3f20d68b7d2f498fa2728cbba4a0a
Mujib517 <Mujib517@gmail.com>
2018-05-21 08:56:23 +0530

    ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
+//directives
+//ngIf, ngFor, ngClass, ngStyle, ngSwitch
+//structural directive
 import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-root',
     template: `
-            <h1>Hello Angular</h1> 
+    <h1>Hello Angular</h1> 
+    <div [hidden]="show">
+        ......
+        <h1 >{{name}}</h1>
+    </div>
 
-            <h1>{{name}}</h1>
+    <input type="text" [(ngModel)]="name"/>
 
-            <input type="text" [(ngModel)]="name"/>
+    <button (click)="toggle()">Toggle</button>
     `
 })
 export class AppComponent {
     name: string = "John";
+    show: boolean = true;
+
+    toggle() {
+        //this.show = this.show ? false : true;
+        this.show = !this.show;
+    }
 }
