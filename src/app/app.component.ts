426e97f004f83fc45d4281fb5ddf0a4678b4b2e3
Mujib517 <Mujib517@gmail.com>
2018-05-21 08:36:53 +0530

    Property, class and Style binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,40 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-root',
-    template: `<h1>Hello Angular</h1>  
+    template: `
     
-       <h1> {{1+2}}  </h1>
+        <h1 [class.red]="name">Hello Angular</h1> 
+        
+        <h1 [style.border]="name?'1px solid gray':'none'">My Heading</h1>
+    
+       <h1 > {{1+2}}  </h1>
        <h1>{{"Mujib" + " Rahman"}}</h1>
        <h2>{{getName()}}</h2>
        {{obj}}
 
        {{obj?"True":"False"}}
+
+       <button (click)="onSave('data')">Save</button>
+       <button [hidden]="false">Save2</button>
    
+       <input type="text" [value]="obj?'True':'False'"/>
     `
 })
 export class AppComponent {
 
-    name: string = "John";
-    obj ="";
+    name: string = "adkfjalkjdf";
+    obj = "";
     surname: string = " Labash";
 
     getName(): string {
         //time consuming
         //no side effects
+        //pure function
         return this.name + ' ' + this.surname;
+    }
+
+    onSave(param) {
+        console.log("Data Saved!!", param);
     }
 
 }
