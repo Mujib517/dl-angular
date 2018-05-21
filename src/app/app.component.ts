52701273a3f075e8f658cbeca7ce7dfc30e051ad
Mujib517 <Mujib517@gmail.com>
2018-05-21 09:04:22 +0530

    ngFor Directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from "@angular/core";
     selector: 'app-root',
     template: `
     <h1>Hello Angular</h1> 
-    <div [hidden]="show">
+    <div *ngIf="getStatus()">
         ......
         <h1 >{{name}}</h1>
     </div>
@@ -15,11 +15,42 @@ import { Component } from "@angular/core";
     <input type="text" [(ngModel)]="name"/>
 
     <button (click)="toggle()">Toggle</button>
+
+    <ul>
+        <li *ngFor="let val of arr">{{val}}</li>
+    </ul>
+
+    <table border="1">
+        <thead>
+            <tr>
+                <td>
+                    Value
+                </td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr *ngFor="let val of arr">
+                <td>{{val}}</td>
+            </tr>
+        </tbody>
+    </table>
+
+
+
+
     `
 })
 export class AppComponent {
     name: string = "John";
     show: boolean = true;
+    arr: number[] = [1, 2, 3, 4, 5, 6];
+    //arr:Array<number>=[1,2,3,4];
+
+
+    getStatus() {
+        //logic
+        return this.show;
+    }
 
     toggle() {
         //this.show = this.show ? false : true;
