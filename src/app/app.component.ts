9f29f6b7f7023bc83dbfccb4b5feac3b61fd155b
Mujib517 <Mujib517@gmail.com>
2018-05-22 08:27:12 +0530

    NgStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,13 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'my-app',
     template: `
-     <h1 [ngClass]="cls">Hello Angular</h1> 
+     <h1 [ngStyle]="stl">Hello Angular</h1> 
     `
 })
 export class AppComponent {
-    // cls: string[] = ["fg" ,"bg"];
-    myProp: boolean=true;
-
-    cls: any = {
-        fg: true,
-        bg: this.myProp
+    stl: any = {
+        border: '1px solid black',
+        color: 'green',
+        'background-color': 'yellow'
     };
 }
