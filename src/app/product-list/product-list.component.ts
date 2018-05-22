68d5da5a07d4368f9761f42615a1b3c99e8f3d63
Mujib517 <Mujib517@gmail.com>
2018-05-22 09:02:12 +0530

    Pipes

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,18 +1,22 @@
 import { Component } from "@angular/core";
 
 // snake casing
+// pipes: lowercase, uppercase, tiltecase, currency, date, json, async
 @Component({
   selector: 'app-product-list',
   template: `
   <h1>Products</h1>
 
   <div *ngFor="let product of products">
-    <h3>{{product.brand}}</h3>
-    <h3>{{product.model}}</h3>
-    <span>{{product.price}}</span>
+    <h3>{{product.brand | uppercase}}</h3>
+    <h3>{{product.model | lowercase}}</h3>
+    <b>{{product.price | currency}}</b>
     <div [ngClass]="product.inStock? 'fg2':'fg'">
       InStock: 
       <input type="checkbox" [disabled]="true" [checked]="product.inStock"/>
+    </div>
+    <div>
+      {{product.lastUpdated | date:'MM-dd-yyyy hh:mm a'}}
     </div>
     <hr/>
   </div>
@@ -25,9 +29,9 @@ export class ProductListComponent {
   products: any[];
 
   constructor() {
-    this.products = [{ id: 1, brand: 'Nokia', model: 'N8', price: 100, inStock: true },
-    { id: 2, brand: 'Samsung', model: 'Galaxy S9', price: 600, inStock: true },
-    { id: 3, brand: 'Apple', model: 'Iphone X', price: 1000, inStock: false }];
+    this.products = [{ id: 1, brand: 'Nokia', model: 'N8', price: 100, inStock: true,lastUpdated:Date.now() },
+    { id: 2, brand: 'Samsung', model: 'Galaxy S9', price: 600, inStock: true,lastUpdated:Date.now() },
+    { id: 3, brand: 'Apple', model: 'Iphone X', price: 1000, inStock: false,lastUpdated:Date.now() }];
   }
 
 
