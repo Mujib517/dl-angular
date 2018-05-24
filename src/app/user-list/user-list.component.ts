1e0f20430933f0c0de98312b2b6c0bb575eb82dc
Mujib517 <Mujib517@gmail.com>
2018-05-24 08:38:33 +0530

    Error handling

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -8,6 +8,9 @@ import { HttpClient } from '@angular/common/http';
     <h1>Users</h1>
 
     <div class="col-md-5">
+    <div *ngIf="hasError" class="alert alert-danger">Failed to load data. Please try again</div>
+      
+    
       <div *ngFor="let user of users" class="well">
         <h3>{{user.login}}</h3>
         <img [src]="user.avatar_url" width="120" height="120" class="img img-circle"/>
@@ -17,6 +20,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class UserListComponent {
 
+  hasError: boolean;
   users: any;
 
   constructor(http: HttpClient) {
@@ -24,7 +28,7 @@ export class UserListComponent {
     http.get("https://api.github.com/users")
       .subscribe(
         res => this.users = res, //success
-        err => console.log(err), //error
+        err => this.hasError=true, //error
         () => console.log("Completed")
       );
   }
