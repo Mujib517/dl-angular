aa5b95ff6d93a6135ba00a6404da28c4d31b13f7
Mujib517 <Mujib517@gmail.com>
2018-05-28 09:06:49 +0530

    RouterLink

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,10 +9,10 @@ import { Component } from '@angular/core';
           My Cool App
       </div>
       <ul class="navbar-nav nav">
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/products">Products</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><a routerLink="/">Home</a></li>
+          <li><a routerLink="/about">About</a></li>
+          <li><a routerLink="/products">Products</a></li>
+          <li><a routerLink="/contact">Contact</a></li>
       </ul>
   </nav>
 </header>
