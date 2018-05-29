061c13b1b239c6325d6a3bd3fbdd1e4c8caa665a
Mujib517 <Mujib517@gmail.com>
2018-05-29 08:37:53 +0530

    Fix null ref exception using *ngIF

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,24 +1,25 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../shared/product.service';
-//safe nav op
+
 @Component({
   selector: 'app-product-detail',
   template: `
     <h1>Product Detail</h1> 
 
-    <div class="panel panel-primary">
-      <div class="panel-heading">
-        <h4>{{product?.brand | uppercase }} {{product?.model}}</h4>
-      </div>
-      <div class="panel-body">
-        <div>{{product?.price | currency}}</div>
-        <div> InStock: {{product?.inStock ? "Yes" : "Out of stock" }}</div>
-      </div>
-      <div class="panel-footer">
-        {{product?.lastUpdated | relativetime}}
+    <div *ngIf="product">
+      <div class="panel panel-primary">
+        <div class="panel-heading">
+          <h4>{{product.brand | uppercase }} {{product.model}}</h4>
+        </div>
+        <div class="panel-body">
+          <div>{{product.price | currency}}</div>
+          <div> InStock: {{product.inStock ? "Yes" : "Out of stock" }}</div>
+        </div>
+        <div class="panel-footer">
+          {{product.lastUpdated | relativetime}}
+        </div>
       </div>
     </div>
-
   `
 })
 export class ProductDetailComponent {
