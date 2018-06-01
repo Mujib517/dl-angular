e21dcb9d7b083f39595b53d4bbcd94ef075669a6
Mujib517 <Mujib517@gmail.com>
2018-06-01 08:57:37 +0530

    Refactored

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import  { SharedModule } from "./shared/shared.module";
+import { SharedModule } from "./shared/shared.module";
 import { AppRouterModule } from "./app.router.module";
 
 import {
@@ -14,17 +14,17 @@ import {
     NewProductComponent, ReactiveFormComponent
 } from './app.barrel';
 
+const COMP_IMPORTS = [AppComponent, HomeComponent,
+    AboutComponent, ProductListComponent,
+    ProductComponent, HeaderComponent,
+    FooterComponent, ContactComponent, NotFoundComponent,
+    ProductDetailComponent, SpecsComponent, ReviewsComponent, LazyComponent,
+    NewProductComponent, ReactiveFormComponent];
+
 //ES6 Module, Angular Module
 @NgModule({
     imports: [BrowserModule, SharedModule, AppRouterModule, FormsModule, ReactiveFormsModule],
-    declarations: [AppComponent, HomeComponent,
-        AboutComponent, ProductListComponent,
-        ProductComponent, HeaderComponent,
-        FooterComponent, ContactComponent, NotFoundComponent,
-        ProductDetailComponent, SpecsComponent, ReviewsComponent, LazyComponent,
-        NewProductComponent, ReactiveFormComponent],
-    providers: [],
-    exports: [],
+    declarations: [...COMP_IMPORTS],
     bootstrap: [AppComponent]
 })
 export class AppModule {
