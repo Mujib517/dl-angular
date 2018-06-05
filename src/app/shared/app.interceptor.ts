b1ae3199e4445879367d435d5330340a8d8d8adf
Mujib517 <Mujib517@gmail.com>
2018-06-05 08:30:38 +0530

    Refactored

--- src/app/shared/app.interceptor.ts
+++ src/app/shared/app.interceptor.ts
@@ -1,14 +1,17 @@
 import { Injectable } from "@angular/core";
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
 import { Observable } from "rxjs/internal/Observable";
+import { UserService } from "./user.service";
 // http//example-dev.com
 //example-stage.com
 
 @Injectable()
 export class AppInterceptor implements HttpInterceptor {
 
+  constructor(private userSvc: UserService) { }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    var hdrs = { 'authorization': localStorage.getItem("token") };
+    var hdrs = { 'authorization': this.userSvc.getToken() };
 
     let newReq = req.clone({ setHeaders: hdrs });
 
